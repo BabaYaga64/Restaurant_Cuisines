2e0e0a4419fb99788e0911087c2cf4bd9752ede1
Tom Mertz & Bojana Skarich <student@epicodus.com>
2015-03-18 15:38:03 -0700

    Add deleteAll test for Cuisine

--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -7,10 +7,16 @@
 
     require_once "src/Cuisine.php";
 
-    $DB = new PDO('pgsql:host=localhost;dbname=restaurant_db');
+    $DB = new PDO('pgsql:host=localhost;dbname=restaurant_test');
 
     class CuisineTest extends PHPUnit_Framework_TestCase
     {
+
+        //clear the database between tests;
+        function tearDown()
+        {
+            Cuisine::deleteAll();
+        }
 
         function test_save()
         {
@@ -24,6 +30,27 @@
             //Assert
             $result = Cuisine::getAll();
             $this->assertEquals($test_cuisine, $result[0]);
+        }
+
+        function test_deleteAll()
+        {
+            //arrange
+            $food = "Mexican";
+            $food2 = "Egytian";
+
+            $test_cuisine = new Cuisine($food);
+            $test_cuisine2 = new Cuisine($food2);
+
+            $test_cuisine->save();
+            $test_cuisine2->save();
+
+            //Act
+            Cuisine::deleteAll();
+
+            //assert
+            $result = Cuisine::getAll();
+            $this->assertEquals([], $result);
+
 
         }
     }
