3cef3760e244bbfb2aa6438f05aaf7664ab22f3b
Tom Mertz & Bojana Skarich <student@epicodus.com>
2015-03-18 15:38:32 -0700

    Add save, deleteAll

--- src/Cuisine.php
+++ src/Cuisine.php
@@ -24,18 +24,27 @@
         function save()
         {
 
+            $GLOBALS['DB']->exec("INSERT INTO cuisine (food_type) VALUES ('{$this->getFoodType()}');");
         }
 
         static function getAll()
         {
-            
+            $returned_cuisines = $GLOBALS['DB']->query("SELECT * FROM cuisine;");
+            $cuisine_array = array();
+            foreach($returned_cuisines as $cuisine) {
+                $food_type = $cuisine['food_type'];
+                $new_cuisine = new Cuisine($food_type);
+                array_push($cuisine_array, $new_cuisine);
+            }
+
+            return $cuisine_array;
         }
 
 
-
-
-
-
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM cuisine *");
+        }
     }
 
 
